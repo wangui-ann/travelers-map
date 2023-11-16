b4235e81df0cba024f0eaf9c260b76b7e6b274cd
Ann <93719602+wangui-ann@users.noreply.github.com>
2023-11-16 15:44:05 +0300

    Create main page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+      <Footer />
+    </>
+  )
+}
